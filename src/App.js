b858436c7a912ff514b4fd564ebb995d693ba51b
profgreatwonder <muoghaluchidinma@gmail.com>
2022-12-19 12:00:48 +0100

    added social media icon and adjust the hero image

--- src/App.js
+++ src/App.js
@@ -1,4 +1,5 @@
 // import Navbar from "./components/Navbar";
+import { InstagramSMIcon, WhatsAppSMIcon } from "./components/Icons";
 import Home from "./pages/Home";
 
 function App() {
--- src/pages/Home.js
+++ src/pages/Home.js
@@ -3,6 +3,12 @@ import {
 	WhiteBgPurchaseChoiceBtn,
 } from "../components/Buttons";
 import Navbar from "../components/Navbar";
+import whatsapp from "../images/social_media_icon/whatsapp.svg";
+import instagram from "../images/social_media_icon/instagram.svg";
+import facebook from "../images/social_media_icon/facebook.svg";
+import linkedin from "../images/social_media_icon/linkedin.svg";
+import email from "../images/social_media_icon/mail.svg";
+import { SocialMediaIcon } from "../components/Icons";
 
 import homepage from "../images/heroImages/homepage_hero_bg.png";
 
@@ -10,18 +16,30 @@ const Home = () => {
 	// push to github and deploy
 	// mobile view
 	return (
-		<div className="HomeBg">
-			<Navbar />
-			<Hero />
+		<div className="HomeBg container">
+			<div className="row">
+				<div className="">
+					<SocialMediaLinks />
+				</div>
+				<div className="">
+					<Navbar />
+					<Hero />
+				</div>
+			</div>
+			{/* <div className="">
+				<Navbar />
+				<Hero />
+			</div>
+			<SocialMediaLinks /> */}
 		</div>
 	);
 };
 
 const Hero = () => {
 	return (
-		<div className="ms-4 my-5 py-5">
+		<div className="ms-4 my-5 py-5 ps-3">
 			{/* <div className="row"> */}
-			<div className="col-md-6 col-sm-12">
+			<div className="col-md-8 ">
 				<div className="w-75">
 					<p className="text-black mb-0">LOREM IPSUM</p>
 
@@ -52,9 +70,20 @@ const Hero = () => {
 					/>
 				</div>
 			</div>
+		</div>
+		// </div>
+	);
+};
 
-			<div className="col-md-6"></div>
-			{/* </div> */}
+const SocialMediaLinks = () => {
+	return (
+		<div className="d-flex flex-column position-fixed top-50 start-0">
+			{/* <div className="d-flex flex-column social_links"> */}
+			<SocialMediaIcon icon={whatsapp} altText="whatsapp icon" />
+			<SocialMediaIcon icon={instagram} altText="instagram icon" />
+			<SocialMediaIcon icon={facebook} altText="facebook icon" />
+			<SocialMediaIcon icon={linkedin} altText="linkedin icon" />
+			<SocialMediaIcon icon={email} altText="email icon" />
 		</div>
 	);
 };
